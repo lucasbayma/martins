d037d26a8968cb5e36a9ed8c04740e864961e19a
Lucas Bayma <bayma@ip-192-168-101-6.ec2.internal>
2026-04-18 11:51:03 -0300

    chore: update formula sha256 for v0.2.0

--- Formula/martins.rb
+++ Formula/martins.rb
@@ -5,7 +5,7 @@ class Martins < Formula
   license "MIT"
 
   url "https://github.com/lucasbayma/martins/releases/download/v#{version}/martins-aarch64-apple-darwin.tar.gz"
-  sha256 "84570c01e4647d54f0e71ebc7d139e9955e071ea39f2f6f932fecf3507f5e8ca"
+  sha256 "e7fdebce979f519d145113f3eeffb0eb57153a6746f5226d55bbc9d850d129af"
 
   depends_on :macos
   depends_on "tmux"
