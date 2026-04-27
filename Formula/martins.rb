5de734f3ea68886333e884f91922bf44d882f132
Lucas Bayma <lucasobayma@gmail.com>
2026-04-27 12:13:55 -0300

    chore: update formula sha256 for v0.8.0

--- Formula/martins.rb
+++ Formula/martins.rb
@@ -5,7 +5,7 @@ class Martins < Formula
   license "MIT"
 
   url "https://github.com/lucasbayma/martins/releases/download/v#{version}/martins-macos-universal"
-  sha256 "4d0a78210728d83edfedc51ce3b4fca14bb4f1c4bb962113a58aea6715b099b8"
+  sha256 "23e1e6394f80fb6eb6713f429acd3be5404a621a658e35e2bfa58ec1919cffac"
 
   depends_on :macos
   depends_on "tmux"
